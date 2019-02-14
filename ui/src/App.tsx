9fd3e4917d5586e5f64cd2feecec5b33416e33a1
Rakesh Ravuri <rravuri@gmail.com>
2019-02-14 09:43:41 +0530

    Server init (#2)
    * server that servers the ui based on react
    
    * updated prefab glyphs
    
    * removed vendor
    
    * added vendor to ignore list
    
    * Update manifest.json

---
+++ ui/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>&#x25E4;&#x25E3;</h1>
+          <h2>
+            prefab
+          </h2>
+          <small>fastest way to get started</small>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
