18d1c154a59e8e548b2ec07a2a2897786f48efad
Mohd Imran <mohimran@WKMIN4269080.global.publicisgroupe.net>
2019-02-28 21:04:08 +0530

    Fixed form controls spacing issue and label selection

--- ui/src/components/configure/CiCdSetup.tsx
+++ ui/src/components/configure/CiCdSetup.tsx
@@ -12,7 +12,6 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const hostingProviders = [
@@ -44,15 +43,13 @@ const CiCdSetup = (props: any) => {
   const [qualityReport, setQualityReport] = useState('speedy');
   const [codeReview, setCodeReview] = useState('crucible');
 
-
-  const onLoggingChange = (event:any) => setLogging(event.target.value);
-  const onQualityReportChange = (event:any) => setQualityReport(event.target.value);
-  
+  const onLoggingChange = (event: any) => setLogging(event.target.value);
+  const onQualityReportChange = (event: any) => setQualityReport(event.target.value);
 
   return (
     <MuiThemeProvider theme={psTheme}>
       <h1>CI-CD Setup</h1>
-      <FormControl className="form-container">
+      <FormControl className="form-container" fullWidth>
         <TextField
           id="hosting-provider"
           select
@@ -68,39 +65,37 @@ const CiCdSetup = (props: any) => {
             </MenuItem>
           ))}
         </TextField>
+      </FormControl>
 
+      <FormControl className="form-container" fullWidth>
         <FormLabel>Select the Environements you need</FormLabel>
         <FormGroup row>
           <FormControlLabel control={<Checkbox value="Develop" />} label="Develop" />
           <FormControlLabel control={<Checkbox value="Staging" />} label="Staging" />
           <FormControlLabel control={<Checkbox value="Production" />} label="Production" />
         </FormGroup>
-        
-        <FormGroup row />
-       
+      </FormControl>
+
+      <FormControl className="form-container" fullWidth>
         <FormLabel>Select the logging Framework</FormLabel>
-        <RadioGroup
-            aria-label="Logging"
-            name="logging"
-            value={logging}
-            onChange={onLoggingChange}
-          >
-           <FormControlLabel value="elk" control={<Radio />} label="ELK" />
-           <FormControlLabel value="efk" control={<Radio />} label="EFK" />
-
+        <FormGroup row>
+          <RadioGroup aria-label="Logging" name="logging" value={logging} onChange={onLoggingChange}>
+            <FormControlLabel value="elk" control={<Radio />} label="ELK" />
+            <FormControlLabel value="efk" control={<Radio />} label="EFK" />
           </RadioGroup>
+        </FormGroup>
+      </FormControl>
 
-        <FormGroup row />
+      <FormControl className="form-container" fullWidth>
         <FormLabel>Location for your Engineering Quality Results</FormLabel>
-        
+
         <RadioGroup aria-label="quality-reports" name="quality-reports" value={qualityReport} onChange={onQualityReportChange}>
-        
-            <FormControlLabel value="speedy" control={<Radio />} label="Speedy" />
-            <FormControlLabel value="s3" control={<Radio />} label="S3" />
-        
+          <FormControlLabel value="speedy" control={<Radio />} label="Speedy" />
+          <FormControlLabel value="s3" control={<Radio />} label="S3" />
         </RadioGroup>
+      </FormControl>
 
-
+      <FormControl className="form-container" fullWidth>
         <TextField
           id="code-reviews"
           select
@@ -116,10 +111,10 @@ const CiCdSetup = (props: any) => {
             </MenuItem>
           ))}
         </TextField>
-        <Button variant="contained" color="primary">
-          Setup Pipeline{' '}
-        </Button>
       </FormControl>
+      <Button variant="contained" color="primary">
+        Setup Pipeline{' '}
+      </Button>
     </MuiThemeProvider>
   );
 };
